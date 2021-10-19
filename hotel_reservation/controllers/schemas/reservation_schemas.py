d875bdb1ca5f041bfaac1872d9bd7e6fe1ff7494
noelalbertodg86 <noelalbertodg86@gmail.com>
2021-10-19 10:04:30 -0500

    Added validator for dates

--- hotel_reservation/controllers/schemas/reservation_schemas.py
+++ hotel_reservation/controllers/schemas/reservation_schemas.py
@@ -1,7 +1,8 @@
-from datetime import date
+from datetime import date, timedelta
 from typing import List, Optional
 
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, validator
 
 from hotel_reservation.controllers.schemas.guest_schema import GuestSchema
 from hotel_reservation.models.models import Reservation
@@ -17,6 +18,18 @@ class ReservationBase(BaseModel):
 class ReservationRequestSchema(ReservationBase):
     class Config:
         orm_mode = True
+
+    @validator("dates")
+    def validate_dates(cls, dates: List[date]):
+        if len(dates) == 0:
+            raise HTTPException(status_code=400, detail="Reservation dates must have at least 1 element")
+        dates.sort()
+        for i in range(len(dates) - 1):
+            if dates[i] + timedelta(days=1) != dates[i + 1]:
+                raise HTTPException(status_code=400, detail="Reservation dates must be consecutive")
+        return dates
+
+
 
 
 class ReservationCreatedSchema(ReservationBase):
